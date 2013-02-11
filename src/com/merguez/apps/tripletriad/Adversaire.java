1d5e6e2a915e8a977fc2a16ead73685f4ab3f0a9
Flow <gokudarkaqw@gmail.com>
2013-02-11 23:38:47 +0100

    Cacaaaaaa tout mouuuuuuuu Commencement listeAdversaire

---
+++ src/com/merguez/apps/tripletriad/Adversaire.java
@@ -0,0 +1,10 @@
+package com.merguez.apps.tripletriad;
+
+public class Adversaire {
+
+	public String nom;
+	
+	public Adversaire(String nom) {
+		this.nom = nom;
+	}
+}
--- src/com/merguez/apps/tripletriad/Jouer.java
+++ src/com/merguez/apps/tripletriad/Jouer.java
@@ -1,20 +1,66 @@
 package com.merguez.apps.tripletriad;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.merguez.apps.tripletriad.MesCartes.CardAdapter;
+import com.merguez.apps.tripletriad.cards.Card;
+import com.merguez.apps.tripletriad.data.DatabaseStream;
 
-public class Jouer extends SherlockFragment {
-	
+public class Jouer extends SherlockListFragment {
+
+	private Context context;
+	private CardAdapter adapter;
+	private ArrayList<Object> listeAdversaires;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
 		View view = inflater.inflate(R.layout.jouer, container, false);
+
 		return view;
 	}
-   
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		context = getActivity().getApplicationContext();
+
+		listeAdversaires = new ArrayList<Object>();
+
+		DatabaseStream dbs = new DatabaseStream(context);
+		dbs.load();
+
+
+		ArrayList<Adversaire> adversaires = new ArrayList<Adversaire>();
+		for (int j = 0; j < 12; j++) {
+			adversaires.add(new Adversaire("Merguez"));
+		}
+
+		
+		listeAdversaires.add("CPU");
+		listeAdversaires.addAll(adversaires);
+
+
+
+		adapter = new CardAdapter(context, listeAdversaires); 
+		setListAdapter(adapter);
+
+		getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+	
+	}
+
+
+
 }
